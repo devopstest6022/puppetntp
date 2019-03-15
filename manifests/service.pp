0aafea46fbe1edaf64c0be857c04719bc6eac2a1
root <root@shashiudawa1c.mylabserver.com>
2019-03-15 03:39:38 +0000

    first commit

---
+++ manifests/service.pp
@@ -0,0 +1,15 @@
+class ntp::service(
+  String $service_name        = $::ntp::service_name,
+  Boolean $service_enable     = $::ntp::service_name,
+  Boolean $service_ensure     = $::ntp::service_ensure,
+  Boolean $service_hasstatus  = $::ntp::service_hasstatus,
+  Boolean $service_hasrestart = $::ntp::service_hasrestart,
+) {
+  service { 'ntp': 
+    ensure     => $service_ensure,
+    name       => $serivce_name,
+    enable     => $service_enable,
+    hasstatus  => $service_hasstatus,
+    hasrestart => $service_hasrestart,
+  }
+}
