b8e2adf0923781a2389df6ec1bea7d3b5dc4587e
root <root@shashiudawa1c.mylabserver.com>
2019-03-15 03:48:07 +0000

    fix bugs

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,13 +1,13 @@
 class ntp::service(
   String $service_name        = $::ntp::service_name,
-  Boolean $service_enable     = $::ntp::service_name,
-  Boolean $service_ensure     = $::ntp::service_ensure,
+  Boolean $service_enable     = $::ntp::service_enable,
+  String  $service_ensure     = $::ntp::service_ensure,
   Boolean $service_hasstatus  = $::ntp::service_hasstatus,
   Boolean $service_hasrestart = $::ntp::service_hasrestart,
 ) {
   service { 'ntp': 
     ensure     => $service_ensure,
-    name       => $serivce_name,
+    name       => $service_name,
     enable     => $service_enable,
     hasstatus  => $service_hasstatus,
     hasrestart => $service_hasrestart,
